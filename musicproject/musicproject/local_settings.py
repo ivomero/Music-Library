f34257ec830a38129950545bc287ffe3e4071eff
Ian Vomero <ivomero@ymail.com>
2021-11-05 11:02:15 -0400

    init

---
+++ musicproject/musicproject/local_settings.py
@@ -0,0 +1,16 @@
+
+SECRET_KEY = 'django-insecure-c*a&w$z6k5-lbh6h2lf8cz@1788#xaznzh%61bxz5vv$5savea'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'music_library_database',
+        'USER': 'root',
+        'PASSWORD': 'Projects4949*',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}
